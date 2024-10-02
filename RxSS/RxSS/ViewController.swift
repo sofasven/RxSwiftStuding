1206a9fbc0376cdeae0a9b214b68d2701aef501d
Sofa <sofasven00@gmail.com>
2024-10-02 21:59:18 +0300

    Project created

---
+++ RxSS/RxSS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSS
+//
+//  Created by Sofa on 2.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
